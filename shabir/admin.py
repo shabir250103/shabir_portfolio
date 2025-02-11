018ce2a5b45ac832d86e9bb717bd91f004effd37
shabir250103 <srpvt21@gmail.com>
2025-02-11 23:58:19 +0530

    Update admin.py

--- shabir/admin.py
+++ shabir/admin.py
@@ -1,3 +1,18 @@
-from django.contrib import admin
+
 
 # Register your models here.
+from django.contrib import admin
+from .models import Projects, ProjectImage,Contact
+
+class ProjectImageInline(admin.TabularInline):
+    model = ProjectImage
+    extra = 1
+
+class ProjectsAdmin(admin.ModelAdmin):
+    inlines = [ProjectImageInline]
+
+admin.site.register(Projects, ProjectsAdmin)
+
+
+
+admin.site.register(Contact)
