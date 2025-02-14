f2b4692092031ae007c2bc1d008a02c6812ae2b5
shabir250103 <srpvt21@gmail.com>
2025-02-14 23:35:59 +0530

    Update urls.py

--- shabir/urls.py
+++ shabir/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('mobile_view',views.mobile_view,name="mobile_view"),
     path('desktop_view',views.desktop_view,name="desktop_view"),
+    path('achievements',views.achievements,name="desktop_view"),
     path('project',views.project,name='project'),
     path('contact',views.contact,name='contact'),
-    path('blog', views.blog, name="blog"),
     path('thank-you/', views.thank_you, name='thank_you'),
     path('resume/',views.download_resume,name='download_resume')
 
